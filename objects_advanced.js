a5e5af36a1d45e9e66a9178a5f8e29230ba7fb7e
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2019-09-30 13:14:23 -0400

    adavanced_objects

--- objects_advanced.js
+++ objects_advanced.js
@@ -1,28 +1,104 @@
 //methods 
 
-let rabbit = {};
+// let rabbit = {};
 
-rabbit.speak = function(line){
-    console.log(`The rabbit line says ${line}`)
+// rabbit.speak = function(line){
+//     console.log(`The rabbit line says ${line}`)
+// }
+
+// rabbit.speak("I'm alive")
+
+// function speak(line){
+//     console.log(`The ${this.type} rabbit says '${line}'`)
+// }
+
+// let whiteRabbit = {type: "white", speak}
+// let hungryRabbit = {type: "hungry", speak}
+
+// whiteRabbit.speak("Oh my ears and whiskers" + "how late it's getting!")
+
+// hungryRabbit.speak("I could use a carrot right now")
+
+// speak.call(hungryRabbit, "yeeer")
+// speak.call(whiteRabbit, "I wish I was black")
+
+// function normalize() {
+//     console.log(this.coords.map(n => n / this.length))
+// }
+// normalize.call({coords: [0, 2, 3], length: 5})
+
+
+// prototypes
+let empty = {};
+// console.log(empty.toString)
+// console.log(empty.toString())
+
+// console.log(Object.getPrototypeOf({})== Object.prototype)
+
+// console.log(Object.getPrototypeOf(Object.prototype))
+
+// console.log(Object.getPrototypeOf(Math.max) == Function.prototype)
+
+// console.log(Object.getPrototypeOf([]) == Array.prototype)
+
+// let protoRabbit = {
+//     speak(line) {
+//         console.log(`The ${this.type} rabbit says '${line}'`)
+//     }
+// };
+
+// let killerRabbit = Object.create(protoRabbit);
+// killerRabbit.type = 'killer';
+// killerRabbit.speak('SKREEE!') 
+
+// function makeRabbit(type){
+//     let rabbit = Object.create(protoRabbit)
+//     rabbit.type = type;
+//     return rabbit
+// }
+
+// let bunny = makeRabbit('bunny')
+// console.log(bunny.speak('Im cute'))
+
+// function Rabbit(type){
+//     this.type = type
+// }
+
+// Rabbit.protototype.speak = function(line){
+//     console.log(`This ${this.type} rabbit says '${line}`)
+// }
+
+// let weirdRabbit = new Rabbit("weird")
+
+// console.log(Object.getPrototypeOf(Rabbit) == Function.prototype)
+
+// console.log(Object.getPrototypeOf(weirdRabbit) == Rabbit.prototype)
+
+
+class Rabbit{
+    constructor(type){
+        this.type = type
+    }
+    speak(line){
+        console.log(`This ${this.type} rabbit says '${line}`)
+    }
 }
 
-rabbit.speak("I'm alive")
+let killerRabbit = new Rabbit("killer");
+let blackRabbit = new Rabbit('black')
 
-function speak(line){
-    console.log(`The ${this.type} rabbit says '${line}'`)
-}
 
-let whiteRabbit = {type: "white", speak}
-let hungryRabbit = {type: "hungry", speak}
+let object = new class { getWord() {return "hello";}}
+// console.log(object.getWord())
 
-whiteRabbit.speak("Oh my ears and whiskers" + "how late it's getting!")
+// Rabbit.prototype.teeth = "small";
 
-hungryRabbit.speak("I could use a carrot right now")
+console.log(killerRabbit.teeth)
 
-speak.call(hungryRabbit, "yeeer")
-speak.call(whiteRabbit, "I wish I was black")
+killerRabbit.teeth = "long, sharp, and bloody"
 
-function normalize() {
-    console.log(this.coords.map(n => n / this.length))
-}
-normalize.call({coords: [0, 2, 3], length: 5})
+console.log(killerRabbit.teeth)
+
+console.log(blackRabbit.teeth)
+
+console.log(Rabbit.prototype.teeth)
